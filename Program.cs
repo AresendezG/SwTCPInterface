2b9579746bb1d5051fbb2061bea71137588bf629
Alejandro Resendez <al.resendez.gro@gmail.com>
2022-03-18 11:15:30 -0500

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwTCPInterface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
