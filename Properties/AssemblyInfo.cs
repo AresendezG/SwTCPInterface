00b86a6a89d6de6104b97f437094052e3d7da036
Alejandro Resendez <al.resendez.gro@gmail.com>
2022-03-23 09:17:48 -0500

    Updated properties and add an Icon (Scanworks Logo)

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // La información general de un ensamblado se controla mediante el siguiente 
 // conjunto de atributos. Cambie estos valores de atributo para modificar la información
 // asociada a un ensamblado.
-[assembly: AssemblyTitle("SwTCPInterface")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("SWTCPInterface")]
+[assembly: AssemblyDescription("Scanworks TCP/IP Interface for InterProcess Communication")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Rio Bravo Technologies")]
 [assembly: AssemblyProduct("SwTCPInterface")]
 [assembly: AssemblyCopyright("Copyright ©  2022")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
 // utilizando el carácter "*", como se muestra a continuación:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
